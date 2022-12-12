6d627cee59a747ef1cbe4309786ce41d560c1206
Gaurav-88074 <81903385+Gaurav-88074@users.noreply.github.com>
2022-12-13 04:49:03 +0530

    deploy

--- backend/settings.py
+++ backend/settings.py
@@ -122,23 +122,23 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 # DATABASES = {
 #     'default': {
-#         #'ENGINE': 'django.db.backends.sqlite3',
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'railway',
-#         'USER': 'postgres',
-#         'PASSWORD': 'v7AyA6xQtePaTePSnmhQ',
-#         'HOST': 'containers-us-west-167.railway.app',
-#         'PORT': '5985',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+DATABASES = {
+    'default': {
+        #'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'v7AyA6xQtePaTePSnmhQ',
+        'HOST': 'containers-us-west-167.railway.app',
+        'PORT': '5985',
+    }
+}
 
 
 # Password validation
